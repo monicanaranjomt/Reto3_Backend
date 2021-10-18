a943640fe51f65f7748879b367bf3dd3c54738d6
MONICA VIVIANA NARANJO LOZADA <moninarl@colsubsidio.com>
2021-10-17 19:24:59 -0500

    first commit

---
+++ src/main/java/Reto3VN/Reto3VN/Reto3VnApplication.java
@@ -0,0 +1,13 @@
+package Reto3VN.Reto3VN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto3VnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3VnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Reto3VN/Reto3VN/Reto3VnApplicationTests.java
@@ -0,0 +1,13 @@
+package Reto3VN.Reto3VN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto3VnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
